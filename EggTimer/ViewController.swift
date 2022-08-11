031d35a2dce969aa6579c257c0aa72e1c25911a5
Temirlan Bekkozha <bekkozha01@gmail.com>
2022-08-11 23:21:40 +0600

    Timer logic corrected

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,19 +11,20 @@ import UIKit
 class ViewController: UIViewController {
     
     let eggTime = ["Soft":5,"Medium":7,"Hard":12]
-    
+    var secondsRemaining = 0
+    var timer: Timer?
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-//        print(sender.currentTitle)
-        let hardness = sender.currentTitle!
-        
-//        print(eggTime[hardness]!)
-        var inSec = eggTime[hardness]!*60
-        
-        while inSec > 0 {
-            print(inSec)
-            inSec-=1
+        timer?.invalidate()
+        secondsRemaining = eggTime[sender.currentTitle!]!*60
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsRemaining > 0 {
+                print ("\(self.secondsRemaining) seconds")
+                self.secondsRemaining -= 1
+            } else {
+                Timer.invalidate()
+            }
         }
+        
     }
 }
-  
